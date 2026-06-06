02621d925a05673733f5a575434ab834a09504e8
EnviousView <alejandrocalgab@gmail.com>
2026-06-06 15:14:34 +0800

    "First Commit - LargestNumber Task"

---
+++ LargestNumber.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        LargestNumber largestNumber = new LargestNumber();
+        Scanner nums = new Scanner(System.in);
+        System.out.println("Please enter three numbers: ");
+        
+        int num1 = nums.nextInt();
+        int num2 = nums.nextInt();
+        int num3 = nums.nextInt();
+
+        int largestNum;
+
+        if(largestNumber.equalChecker(num1, num2, num3)) {
+            System.out.println("All numbers are equal");
+        } else {
+            if(num1 > num2 && num1 > num3) {
+                largestNum = num1;
+                // if(num1 > num3) {
+                //     largestNum = num1;
+                // } else {
+                //     largestNum = num3;
+                // }
+            } else if (num2 > num3) {
+                // if(num2 > num3) {
+                //     largestNum = num2;
+                // } else {
+                    largestNum = num2;
+                // }
+            } else {
+                largestNum = num3;
+            }
+            System.out.println("Largest number is: " + largestNum);
+        }
+        nums.close();
+    }
+
+    public boolean equalChecker(int num1, int num2, int num3) {
+        int total;
+
+        total = num1 + num2 + num3;
+        if (total % num1 == 0) {
+            return true;
+        } 
+
+        return false;
+    }
+}
+
